3b2243fd19b1aab8f1a0f6ec1b26c1cd8aff9200
Joao Iguana <jiboiadamor@gmail.com>
2024-09-23 19:14:02 +0100

    refactored my app to use a reusable hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,9 @@
-import { useContext } from 'react';
+import useBooksContext from '../hooks/use-books-context';
 import BookShow from './BookShow';
-import BooksContext from '../context/Books';
+
 
 function BookList() {
-  const { books } = useContext(BooksContext);
+  const { books } = useBooksContext();
 
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />
